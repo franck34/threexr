e16a11b81c406e8d35bcf0087f2b589d234c5e6e
franck34 <franck34@gmail.com>
2021-05-22 21:05:17 +0200

    commit webpack stuff

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'threexr.js',
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns:[
+				{ from: '**/*', to: './', context: "src/static", }
+			]
+		}),
+	],
+};
+
---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		https:true,
+		compress:false,
+		port:3000,
+		hot:true,
+		//publicPath: './src/staticx',
+		liveReload:true,
+		writeToDisk: true,
+		serveIndex: true,
+		//public: './src/static'
+	}
+});
